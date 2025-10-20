83df353dfba3e1855da7ea31212995e2956fe28c
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-20 14:04:31 +0500

    Theme Colors Added

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      App
+    </div>
+  );
+};
+
+export default App;
